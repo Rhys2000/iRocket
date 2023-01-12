8fcd3a79ef2b6c539fd77c7354ca66debd173bd5
Rhys2000 <rhys2kjj@gmail.com>
2023-01-11 16:37:23 -0800

    Began working on the scaleFactor variable to be able to resize content depending on the size of the user's device's dimensions. Also tested .json data to determine whether it was possible to include optionals within .json data for use with a struct

--- iRocket/VesselViewController.swift
+++ iRocket/VesselViewController.swift
@@ -1,7 +1,6 @@
 //
 //  VesselViewController.swift
 //  iRocket
-//
 //  Created by Rhys Julian-Jones on 1/9/23.
 //
 
@@ -13,26 +12,25 @@ class VesselViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //ViewController Title
         title = "Vessels"
-        view.backgroundColor = .blue
-        navigationController?.navigationBar.backgroundColor = .green
-        print(navigationController?.navigationBar.bounds)
-        print(tabBarController?.tabBar.frame.size)
-        // Do any additional setup after loading the view.
-        print(view.bounds)
+        
+        // **** All of these variables should be trandformed into global variables ****
         
         //iPhone 12 Pro Dimensions: Width = 390.0, Height = 844.0 -> NavigationBar Width = 390.0, Height = 96.0, Starting Point (0, 0)
+        
+        //Add together the height of the NavigationBar and TabBar so that we can determine the height of the usable space in the ViewController to displlay content depending on their specific device
+        let topAndBottomMargin = Double((navigationController?.navigationBar.frame.size.height)!) + Double((tabBarController?.tabBar.frame.size.height)!)
+
+        //Set variables to hold the values of both the height and the width of the contentView where information will be displayed to the user
+        let contentViewHeight = Double(view.frame.size.height) - topAndBottomMargin
+        let contentViewWidth = Double(view.frame.size.width)
+        
+        //Variable to scale up or down the size of objects in the view depending on the available content space on the user's device
+        let scaleFactor = contentViewHeight / contentViewWidth
+        
+        print(contentViewHeight, contentViewWidth, scaleFactor)
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
